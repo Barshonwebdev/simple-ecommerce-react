49da8db64794284f11e1c313a5a10b1826f805e5
Barshon <barshonweb@gmail.com>
2023-09-06 21:56:30 +0600

    products info shown with add to cart button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,17 @@
 import './Product.css'
 const Product = (props) => {
-    const {img,name,seller,quantity,price}=props.product;
+    const {img,name,seller,ratings,price}=props.product;
     return (
-        <div className="product">
-            <img src={img} alt="" />
-            <h6>{name}</h6>
+      <div className="product">
+        <img src={img} alt="" />
+        <div className="product-info">
+          <h3 className='product-name'>{name}</h3>
+          <p>Price: ${price}</p>
+          <p>Manufacturer: {seller}</p>
+          <p>Rating: {ratings} stars</p>
         </div>
+        <button className="add-to-cart-button">Add to cart</button>
+      </div>
     );
 };
 
