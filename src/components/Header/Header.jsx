20ebfdd3a4ecaed52c9f79a3d5c259fb0955e027
Barshon <barshonweb@gmail.com>
2023-09-06 14:54:02 +0600

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
-
+import logo from '../../../images/Logo.svg'
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h3>This is a heading</h3>
-        </div>
+        <nav className="header">
+            <img src={logo} alt="" />
+            <div>
+                <a href="/order">Order</a>
+                <a href="/review">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+            
+        </nav>
     );
 };
 
