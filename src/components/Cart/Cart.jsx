1cf91198bbb98396582e82f2aa3766351ec54a96
Barshon <barshonweb@gmail.com>
2023-09-07 11:23:09 +0600

    cart calculations

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
     const {cart}=props;
-    let total=0;
+    let totalPrice=0;
+    let shipping=0;
     for (const cartItem of cart){
-        total=total+cartItem.price;
+        totalPrice=totalPrice+cartItem.price;
+        shipping=shipping+cartItem.shipping;
     }
+    const tax= totalPrice*8/100;
+    const grandTotal=totalPrice+shipping+tax;
     return (
       <div className='cart-container'>
         <h4>Order Summary</h4>
         <p>Products added: {cart.length}</p>
-        <p>Total price: $ {total} </p>
-        <p>Total Shipping Charge: $ </p>
-        <p>Tax: $ </p>
-        <h3>Grand Total: $ </h3>
+        <p>Total price: $ {totalPrice} </p>
+        <p>Total Shipping Charge: $ {shipping} </p>
+        <p>Tax: $ {tax.toFixed(2)} </p>
+        <h3>Grand Total: $ {grandTotal.toFixed(2)} </h3>
       </div>
     );
 };
