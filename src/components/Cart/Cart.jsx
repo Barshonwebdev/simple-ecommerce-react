cef87495b973931ff215e3e6d1f28e44f26f624b
Barshon <barshonweb@gmail.com>
2023-09-08 16:03:58 +0600

    cart quantity updated accurately

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,16 +4,22 @@ const Cart = (props) => {
     const {cart}=props;
     let totalPrice=0;
     let shipping=0;
+    let quantity=0;
     for (const cartItem of cart){
-        totalPrice=totalPrice+cartItem.price;
+       //cartItem.quantity=cartItem.quantity||1;
+      // if(cartItem.quantity===0){
+      //  cartItem.quantity=1;
+      // }
+        totalPrice=totalPrice+cartItem.price*cartItem.quantity;
         shipping=shipping+cartItem.shipping;
+        quantity=quantity+cartItem.quantity;
     }
     const tax= totalPrice*8/100;
     const grandTotal=totalPrice+shipping+tax;
     return (
       <div className='cart-container'>
         <h4>Order Summary</h4>
-        <p>Products added: {cart.length}</p>
+        <p>Products added: {quantity}</p>
         <p>Total price: ${totalPrice} </p>
         <p>Total Shipping Charge: ${shipping} </p>
         <p>Tax: ${tax.toFixed(2)} </p>
