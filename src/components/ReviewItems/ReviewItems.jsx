482754f17e7c7e591b7241745aa4337ed5929e22
Barshon <barshonweb@gmail.com>
2023-09-19 19:53:48 +0600

    order items boxes shown with styles

--- src/components/ReviewItems/ReviewItems.jsx
+++ src/components/ReviewItems/ReviewItems.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import './ReviewItems.css'
+import './ReviewItems.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 const ReviewItems = ({product}) => {
-    const {id,name,img,price}= product;
+    const {id,quantity,name,img,price}= product;
     console.log(product);
     return (
-        <div className='items'>
-            <img src={img} alt="" />
-            <p>{name}</p>
+      <div className="items">
+        <div className='item-elements'>
+          <img src={img} alt="" />
+          <div className="items-info">
+            <h4>{name}</h4>
+            <p>Price: ${price}</p>
+            <p>Quantity: {quantity}</p>
+          </div>
         </div>
+        <div className='dlt'>
+            <button className='dlt-button'><FontAwesomeIcon  icon={faTrash}/></button>
+        </div>
+      </div>
     );
 };
 
