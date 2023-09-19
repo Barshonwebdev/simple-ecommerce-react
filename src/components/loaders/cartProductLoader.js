3e02c7bc982f1098a4b0a916e85bffa887594cdb
Barshon <barshonweb@gmail.com>
2023-09-19 09:49:55 +0600

    common cart

--- src/components/loaders/cartProductLoader.js
+++ src/components/loaders/cartProductLoader.js
@@ -1,8 +1,22 @@
+import { getShoppingCart } from "../../../utilities/fakedb";
+
 const cartProductLoader = async()=>{
     const loadedProducts= await fetch ('products.json');
     const products= await loadedProducts.json();
     console.log(products);
-    return products;
+    const storedCart= getShoppingCart();
+    const savedCart= [];
+
+    for (const id in storedCart){
+        const addedProduct= products.find(pd=>pd.id===id);
+        if (addedProduct){
+            const quantity=storedCart[id];
+            addedProduct.quantity=quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+    console.log(savedCart);
+    return savedCart;
 }
 
 export default cartProductLoader;
